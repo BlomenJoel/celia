517720d7f512cf26ff3bce48b4e13f44a8c012ed
joel Blomén <joelblomen@joels-MacBook-Pro.local>
2025-04-27 16:49:51 +0200

    add speedinsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Alice } from "next/font/google"
 import "./globals.css"
 import Script from "next/script"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Import Alice font
 const alice = Alice({
@@ -86,6 +86,7 @@ export default function RootLayout({
         {children}
 
         {/* Strukturerad data för restaurangen (JSON-LD) */}
+        <SpeedInsights />
         <Script
           id="restaurant-schema"
           type="application/ld+json"
